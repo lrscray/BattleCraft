eec12c0b6b2cbc91777152d660ac76aeee387191
Reyes, Michael <mradianlogs@gmail.com>
2020-04-16 16:16:07 -0500

    Added my work for the building selection and placement tools.

---
+++ Project4/Assets/Scripts/PlayerResourceManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerResourceManager : MonoBehaviour
+{
+    [SerializeField] private int numResources = -1;
+
+    [SerializeField] private Text numResourceLabel = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        numResources = 0;
+        StartCoroutine(IncrementResourcesAfterTime());
+    }
+
+    IEnumerator IncrementResourcesAfterTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            numResources++;
+            numResourceLabel.text = numResources.ToString();
+        }
+    }
+
+    public int GetNumResources()
+    {
+        return numResources;
+    }
+
+    public void BuildBuilding(int buildingCost)
+    {
+        numResources -= buildingCost;
+        numResourceLabel.text = numResources.ToString();
+    }
+
+    public void TakeUpKeep(int upKeepCost)
+    {
+        numResources -= upKeepCost;
+        numResourceLabel.text = numResources.ToString();
+    }
+
+    
+}
