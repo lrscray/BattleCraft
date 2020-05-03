590d7554bbb6b698a71755b73b696005a9feb226
Michael Reyes <mradianlogs@gmail.com>
2020-05-02 20:48:37 -0500

    Started adding object pooling to game.

---
+++ Project4/Assets/Scripts/ManagerScripts/ObjectPoolManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPoolManager : MonoBehaviour
+{
+    //Used to store and find the specific object pool for a specific object.
+    Dictionary<int, Queue<GameObject>> ObjectPoolDictionary = new Dictionary<int, Queue<GameObject>>();
+
+    public void CreateNewObjectPool(GameObject prefab, int poolSize)
+    {
+        
+    }
+}
